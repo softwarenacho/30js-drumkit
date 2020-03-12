397412373a790f906d766bf17fa0e449022b9c2f
Nacho Betancourt <nachobe@Nachos-MacBook-Pro.local>
2020-03-11 18:50:35 -0600

    Added styles and js refactor to accept onclick

---
+++ app.js
@@ -0,0 +1,18 @@
+function playAudio(e) {
+  const code = e.keyCode ? e.keyCode : e.getAttribute("data-key");
+  const audio = document.querySelector(`audio[data-key="${code}"]`);
+  if (audio) {
+    audio.currentTime = 0;
+    audio.play();
+    const key = document.querySelector(`.drum-key[data-key="${code}"]`);
+    key.classList.add('playing')
+  }
+}
+
+function removeTransition(e) {
+  if (e.propertyName === 'transform') this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playAudio);
+var keys = document.querySelectorAll('.drum-key');
+keys.forEach( key => key.addEventListener('transitionend', removeTransition));
